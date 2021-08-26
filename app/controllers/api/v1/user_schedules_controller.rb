1ec139e68132c5012dbe59a0e3a2265ac423e8d0
Gus <gsc13@tuta.io>
2021-08-26 17:15:15 -0300

    Add UserSchedules serializer, Update UserSchedules Controller

--- app/controllers/api/v1/user_schedules_controller.rb
+++ app/controllers/api/v1/user_schedules_controller.rb
@@ -2,7 +2,13 @@ module Api
   module V1
     class UserSchedulesController < ApplicationController
       def index
-        items = User.find(params[:user]).schedules.where(activity_id: params[:activity])
+        user = User.find(params[:user])
+        items = if params[:activity]
+                  schedules = user.schedules.where(activity_id: params[:activity])
+                  UserSchedulesSerializer.new(schedules).as_json
+                else
+                  UserSchedulesSerializer.new(user.schedules, true).as_json_activities
+                end
         render json: items
       end
     end
