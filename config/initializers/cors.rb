6ddb9da68f5ad1d27d530ff0167f3eff085e3b79
Gus <gsc13@tuta.io>
2021-08-31 19:05:46 -0300

    Update CORS config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins "http://localhost:3001", 'http://127.0.0.1:3001'
     resource "*", headers: :any, methods: [:get, :post, :put, :patch,:delete, :options, :head], credentials: true
   end
 
-  # Set production origin here
-  # allow do
-  #   origins "http://localhost:3000"
-  #   resource "*", headers: :any, methods: [:get, :post, :put, :patch,:delete, :options, :head], credentials: true
-  # end
+  allow do
+    origins "https://storm-mountain.netlify.app"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch,:delete, :options, :head], credentials: true
+  end
 end
